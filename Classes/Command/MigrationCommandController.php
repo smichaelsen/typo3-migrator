7923376e9260ccde6ef5dc8f27ccb15911dc7086
Sebastian Michaelsen <sebastian@app-zap.de>
2013-12-13 23:08:54 +0100

    Iterate over sql folder

--- Classes/Command/MigrationCommandController.php
+++ Classes/Command/MigrationCommandController.php
@@ -6,8 +6,39 @@ use TYPO3\CMS\Extbase\Mvc\Controller\CommandController;
 
 class MigrationCommandController extends CommandController {
 
+	/**
+	 * @var \TYPO3\CMS\Core\Registry
+	 * @inject
+	 */
+	protected $registry;
+
+	/**
+	 * @var array
+	 */
+	protected $extensionConfiguration;
+
+	/**
+	 *
+	 */
+	protected function initialize() {
+		$this->extensionConfiguration = unserialize($GLOBALS['TYPO3_CONF_VARS']['EXT']['extConf']['migrator']);
+	}
+
+	/**
+	 *
+	 */
 	public function migrateSqlFilesCommand() {
-		$this->output('yeah');
+		$this->initialize();
+		$sqlFolderPath = realpath(PATH_site . $this->extensionConfiguration['sqlFolderPath']);
+		$iterator = new \DirectoryIterator($sqlFolderPath);
+		$highestExecutedVersion = NULL;
+		foreach ($iterator as $fileinfo) {
+			/** @var $fileinfo \DirectoryIterator */
+			if ($fileinfo->getExtension() === 'sql') {
+				$fileVersion = intval($fileinfo->getBasename('.sql'));
+				$this->output($fileVersion);
+			}
+		}
 	}
 
 }
